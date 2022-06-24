b59fa873efe0704a6418ebc2dc8558ff8b888f45
Zombienoff <107246360+smthingsmur@users.noreply.github.com>
2022-06-24 15:43:41 +0700

    Create L3_InputFunction.py

---
+++ L3_InputFunction.py
@@ -0,0 +1,4 @@
+print("----------------------------------------------------")
+inputData = input("Input something here: ")
+print(inputData + ", dataType:", type(inputData))
+print("----------------------------------------------------")
