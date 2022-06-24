4a16443acec4faaa9d88a7856f863dea6676ced8
Zombienoff <107246360+smthingsmur@users.noreply.github.com>
2022-06-24 15:43:47 +0700

    Create CS01-2.py

---
+++ CS01-2.py
@@ -0,0 +1,4 @@
+print("----------------------------------------------------")
+inputData = input("Input your first name: ")
+print("Your first name is:", inputData + ".")
+print("----------------------------------------------------")
